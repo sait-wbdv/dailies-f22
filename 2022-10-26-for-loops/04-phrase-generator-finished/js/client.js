a82b458648f0f38c9af817c0ec521bec22c1174c
Tony Grimes <acidtone@tonygrimes.com>
2022-10-26 13:04:33 -0600

    added finished phrase generator

--- 2022-10-26-for-loops/04-phrase-generator-finished/js/client.js
+++ 2022-10-26-for-loops/04-phrase-generator-finished/js/client.js
@@ -4,12 +4,21 @@ import { randomInteger } from './utilities.js';
 const form = document.querySelector('form');
 const output = document.querySelector('.output');
 
+
 const handleSubmit = function(event) {
   event.preventDefault();
+  // Start with an empty array with each click
+  let result = [];
 
-  
+  for (let i = 1; i <= form.count.value; i++) {
 
-  output.textContent = nouns[randomInteger(nouns.length)];
+    const randomWord = nouns[randomInteger(nouns.length)];
+    // Add a new word with every iteration
+    result.push(randomWord);
+
+  }
+  // Print the words separated by a space
+  output.textContent = result.join(' ');
 }
 
 form.addEventListener('submit', handleSubmit);
