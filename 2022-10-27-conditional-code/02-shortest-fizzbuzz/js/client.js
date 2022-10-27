a8ea0fd1cc04aab1da0bbfed317a8e2ab59e52b5
Tony Grimes <acidtone@tonygrimes.com>
2022-10-27 10:42:38 -0600

    fixes to fizzbuzz

--- 2022-10-27-conditional-code/02-shortest-fizzbuzz/js/client.js
+++ 2022-10-27-conditional-code/02-shortest-fizzbuzz/js/client.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const fizzbuzzOriginal = [...Array(100)].map((_,i)=>(++i%3?"":"Fizz")+(i%5?"":"Buzz")||i);
+// const fizzbuzzOriginal = [...Array(100)].map((_,i)=>(++i%3?"":"Fizz")+(i%5?"":"Buzz")||i);
 
 const fizzbuzzFormatted = [...Array(100)].map(
-  (_,i)=> // 1. Loop through every item and create a new array
+  (_,i)=> // 1. Loop through every item and create a new array by pushing a new item to fizzbuzzFormatted
   // For each item:
-  // 6. Increment i after lines 5-9 execute
+  // 5. Increment i after lines 10-16 execute
   (++ 
   // 2. If divisible by 3, print Fizz else ""
   i%3?"":"Fizz")
-  // 4. Add Step 1 and 2 into one string
+  // 4. Add Step 2 and 3 into one string
   + 
   // 3. If divisible by 5, print Buzz else ""
   (i%5?"":"Buzz") 
-  // 5. Print i if "" + ""
+  // 6. Print i if "" + "", otherwise print "Fizz", "Buzz" or "FizzBuzz"
   ||
   i); 
 
