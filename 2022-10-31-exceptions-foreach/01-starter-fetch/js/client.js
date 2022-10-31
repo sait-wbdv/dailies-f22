f9e5349655262bcbcf1a27b45896972698132a66
Tony Grimes <acidtone@tonygrimes.com>
2022-10-31 07:09:00 -0600

    added fetch starter

--- 2022-10-31-exceptions-foreach/01-starter-fetch/js/client.js
+++ 2022-10-31-exceptions-foreach/01-starter-fetch/js/client.js
@@ -1,30 +1,9 @@
-const randomInteger = function(max) {
-  const randomInteger = Math.floor(Math.random() * max);
-  return randomInteger;
-}
-
-const fetchData = async function(endpoint) {
-  const response = await fetch(endpoint);
+const fetchData = async function() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await response.json();
 
-  const animal = data[randomInteger(data.length)];
-
-  const body = document.querySelector('body');
-
-  output = `
-    <article>
-      <h2>${animal.name}</h2>
-      <img src="${animal.image_link}" alt="Picture of ${animal.name}">
-      <ul>
-        <li>Diet: ${animal.diet}</li>
-        <li>Habitat: ${animal.habitat}</li>
-        <li>Range: ${animal.geo_range}</li>
-        <li>Avg Weight: ${(parseFloat(animal.weight_min) + parseFloat(animal.weight_max)) / 2} pounds</li>
-        <li>Lifespan: ${animal.lifespan} years</li>
-      </ul>
-    </article>
-    `
-  body.innerHTML = output;
+  console.log(data);
+  document.querySelector('body').innerHTML = `<h1>${data[6].title}</h1>`;
 }
-        
-fetchData('https://zoo-animal-api.herokuapp.com/animals/rand/10');
+
+fetchData();
