0259e6fb7a39216d553d0ff4cfb1966d41b5239f
Tony Grimes <acidtone@tonygrimes.com>
2022-11-14 09:14:57 -0700

    added starter code

--- 2022-11-03-strapi-intro/pub-website/src/api/menu-item/controllers/menu-item.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * menu-item controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::menu-item.menu-item');
--- 2022-11-03-strapi-intro/pub-website/src/api/menu-item/routes/menu-item.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * menu-item router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::menu-item.menu-item');
